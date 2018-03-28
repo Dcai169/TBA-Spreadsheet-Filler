479d4652c1456919fe513fe1df3f13d9effe1687
MrTrainCow <legoguy145@gmail.com>
2018-03-28 12:11:47 -0400

    consolitdated funtions into classes

--- main.py
+++ main.py
@@ -6,59 +6,88 @@ import json
 import pprint
 from oauth2client.service_account import ServiceAccountCredentials
 #variable declaration
-TBA='https://www.thebluealliance.com/api/v3/'
 focusTeam='frc5735'
 focusDistrict='2018ne'
 eventKey='2018marea'
-TBAauthKey="uSAyrK7Xsxf7mSCSY6ivVy4KHM8CpyVISV9EM96d8ZS9ZLCY4oFsyAWPcSaByD2U"
 pp=pprint.PrettyPrinter()
-estimatedPerformance=40
+
 #google autherization
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 gc = gspread.authorize(credentials)
-sh=gc.open('Reading Scouting Data Test').get_worksheet(1)
-headers={'X-TBA-Auth-Key' : TBAauthKey}
+sh=gc.open('Scouting Data Test Sheet').get_worksheet(1)
+
 print("init complete")
 
 #program logic starts here
+class TBA:
+  def __init__(self):
+    self.data = []
+    self.TBAurl='https://www.thebluealliance.com/api/v3/'
+    self.headers={'X-TBA-Auth-Key' : "uSAyrK7Xsxf7mSCSY6ivVy4KHM8CpyVISV9EM96d8ZS9ZLCY4oFsyAWPcSaByD2U"}
+    self.timeout=1
+    #Reads from data from TBA API
+  def reader(self, path):
+    output=json.loads(requests.get(self.TBAurl+path, params=self.headers).text)
+    return output
+  #gets team numbers and nicknames from TBAreader  
+  def getEventParticipants(self, event):
+    eventTeams=self.reader("event/"+str(event)+"/teams/keys")
+    participants={}
+    for i in range(0,len(eventTeams)):
+      participants[str(eventTeams[i][3:])]=(self.reader("team/"+eventTeams[i]))['nickname']
+    return participants
+tba=TBA()
+  
+#searches a dictionary for a key Or Value; returns the index
+#fix this its broken
+class searchDistrictRankings:
+  def __init__(self, district):
+    self.data = []
+    self.rankings=tba.reader("district/"+district+"/rankings")
+    
+  def rankingsKeys(self, query):
+    for i in range(0,len(self.rankings)):
+      if self.rankings[i]==query:
+        return i
+        
+  def indexByKey(self, query, key):
+    for i in range(0,len(self.rankings)):
+      if self.rankings[i]==key:
+        for j in range(0,len(self.rankings[i])):
+          if self.rankings[i][j]==query:
+            return i
+s=searchDistrictRankings("2018ne")
 
-#Reads from data from TBA API
-def TBAreader(path):
-  output=json.loads(requests.get(TBA+path, params=headers).text)
-  return output
+class advancementPrediction:
+  def __init__(self):
+    self.data = []
+  #uses the keenan advancement prediction algorithm
+  def advancementPredictor(self, estimation, district, team):
+    rankings=tba.reader("district/"+district+"/rankings")
+    for k in range(0,len(rankings)):
+      passingTeams={}
+      if rankings[k]['team_key']==team:
+        print("baseline team found")
+        focusTeamRank=rankings[k]['rank']
+        focusTeamRP=rankings[k]['point_total']
+        passingThreshold=k+estimation
+        for l in range(k, passingThreshold):
+          if focusTeamRP>rankings[l]['point_total']:
+            passingTeams[rankings[l]['team_key']]=(rankings[l]['point_total'])
+            return(len(passingTeams)+focusTeamRank+1)
+          
+  def keenanFunction(self, futile):
+    futile=False
+    return futile
+a=advancementPrediction()
 
-#gets team numbers and nicknames from TBAreader  
-def TBAeventParticipantsReader(event):
-  eventTeams=TBAreader("event/"+str(event)+"/teams/keys")
-  participants={}
-  for i in range(0,len(eventTeams)):
-    participants[str(eventTeams[i][3:])]=(TBAreader("team/"+eventTeams[i]))['nickname']
-  return participants
 #writes data to spreadsheet
 def sheetDataWriter(event):
-  participantNumbers=list(TBAeventParticipantsReader(event).keys())
-  participantNames=list(TBAeventParticipantsReader(event).values())
+  participantNumbers=list(tba.getEventParticipants(event).keys())
+  participantNames=list(tba.getEventParticipants(event).values())
   print('writing')
   for j in range(0, len(participantNumbers)):
     sh.update_cell(j+2, 1, participantNumbers[j])
     sh.update_cell(j+2, 2, participantNames[j])
   return True
-#uses the keenan advancement prediction algorithm
-def advancementPredictor(team, district, estimation):
-  rankings=TBAreader("district/"+district+"/rankings")
-  for k in range(0,len(rankings)):
-    passingTeams={}
-    if rankings[k]['team_key']==team:
-      print("baseline team found")
-      focusTeamRank=rankings[k]['rank']
-      focusTeamRP=rankings[k]['point_total']
-      passingThreshold=k+estimation
-      #print(k, focusTeamRank, estimation, passingThreshold)
-      for l in range(k, passingThreshold):
-        if focusTeamRP>rankings[l]['point_total']:
-          passingTeams[rankings[l]['team_key']]=(rankings[l]['point_total'])
-      return(len(passingTeams)+focusTeamRank+1)
-      
-sheetDataWriter("2018marea")
-print(advancementPredictor(focusTeam, focusDistrict, estimatedPerformance))
