65a617817507176e1bd42864fcf59b6d58868c3a
MrTrainCow <legoguy145@gmail.com>
2018-12-26 16:53:04 -0500

    i despise pep8 and others
    added capability to write labels for spectator scouting
    updated for apiaccessor 0.0.1.4
    added percent complete prints

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import pprint
 from oauth2client.service_account import ServiceAccountCredentials
 import apiaccessor
 import credentials
+from time import sleep
+from json import loads
 # variable declaration
-focusTeam = 'frc5735'
-focusDistrict = '2019ne'
-# eventKey = '2019marea'
-eventKey = '2019mabos'
+focus_team = 'frc5735'
+focus_district = '2019ne'
+event_key = '2018marea'
+# event_key = '2019mabos'
+sheet_name = 'Scouting Data Test Sheet'
+delay = 1
 pp = pprint.PrettyPrinter()
 KEY_NAME = 'X-TBA-Auth-Key'
 KEY = credentials.key
@@ -17,7 +21,8 @@ URL = 'https://www.thebluealliance.com/api/v3/'
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 gc = gspread.authorize(credentials)
-sh = gc.open('2019 Greater Boston Scouting Data').get_worksheet(1)
+pws = gc.open(sheet_name).get_worksheet(1)
+sws = gc.open(sheet_name).get_worksheet(2)
 
 print("init complete")
 
@@ -27,22 +32,48 @@ tba = apiaccessor.XAPIKey(URL, KEY_NAME, KEY)
 
 # gets team numbers and nicknames from TBAreader
 def get_event_participants(event):
-    event_teams = tba.reader("event/"+str(event)+"/teams/keys")
+    event_teams = loads(tba.reader("event/"+str(event)+"/teams/keys", '').text)
     participants = {}
-    for i in range(0, len(event_teams)):
-        participants[str(event_teams[i][3:])] = tba.reader("team/"+event_teams[i])['nickname']
+    for i in range(len(event_teams)):
+        participants[str(event_teams[i][3:])] = loads(tba.reader("team/"+event_teams[i], '').text)['nickname']
     return participants
 
 
 # writes data to spreadsheet
-def sheet_data_writer(event):
+def plabel_writer(event):
     participant_numbers = list(get_event_participants(event).keys())
     participant_names = list(get_event_participants(event).values())
     print('writing')
-    for j in range(0, len(participant_numbers)):
-        sh.update_cell(j+2, 1, participant_numbers[j])
-        sh.update_cell(j+2, 2, participant_names[j])
-    return True
+    for j in range(len(participant_numbers)):
+        pws.update_cell(j + 2, 1, participant_numbers[j])
+        pws.update_cell(j + 2, 2, participant_names[j])
+        print(str(round(calc_percent(j, len(participant_numbers))))+'% complete', end='\r')
+        sleep(delay)
+    print()
 
 
+def slabel_writer(event):
+    matches = loads(tba.reader('event/'+event+'/matches/simple', '').text)
+    all_participants = []
+    for i in range(len(matches)):
+        allianceb = matches[i]['alliances']['blue']['team_keys']
+        alliancer = matches[i]['alliances']['red']['team_keys']
+        for team in allianceb:
+            all_participants.append(team)
+        for team in alliancer:
+            all_participants.append(team)
+    print('writing')
+    for j in range(len(all_participants)):
+        sws.update_cell(j + 2, 2, all_participants[j][3:])
+        print(str(round(calc_percent(j, len(all_participants))))+'% complete', end='\r')
+        sleep(delay)
+    print()
 
+
+def calc_percent(part, whole):
+    return part/whole*100
+
+
+# plabel_writer(event_key)
+# slabel_writer(event_key)
+print('done')
